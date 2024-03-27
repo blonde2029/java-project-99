ae3fd670c9267f32d27738f9171268d56aeeb8f6
blonde2029 <mr_blonde@rambler.ru>
2024-03-27 19:37:39 +0700

    fixed bugs: 1. removed java plugin in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
-	java
 	application
 	jacoco
 	id("checkstyle")
